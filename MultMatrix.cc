86375ab598d69ff4d890a944d34f64e882006f16
TheIszer <theiszer@gmail.com>
2022-05-13 01:29:01 -0400

    update

--- MultMatrix.cc
+++ MultMatrix.cc
@@ -18,8 +18,10 @@
 void MultMatrix::DOijk(const Matrix<float>& A,const Matrix<float>& B, Matrix<float>& C)
 {
 	//Completar
-
-
+	std::cout << "Hola piter\n";
+	std::cout << "rows: "<< &A.rows() << std::endl;
+	std::cout << "columms: "<< A.cols() << std::endl;
+	
 
 	/*for(size_t i=0; i < filas(a); i++){
 		for(size_t j=0; j < columnas(b); j++){
