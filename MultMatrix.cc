04f02d6740a9f12ba75589c1a327c7f2d59730d8
TheIszer <theiszer@gmail.com>
2022-05-13 03:28:31 -0400

    update

--- MultMatrix.cc
+++ MultMatrix.cc
@@ -15,27 +15,29 @@
 
 
 
-void MultMatrix::DOijk(const Matrix<float>& A,const Matrix<float>& B, Matrix<float>& C)
+/*void MultMatrix::DOijk(const Matrix<float>& A,const Matrix<float>& B, Matrix<float>& C)
 {
 	//Completar
+	int i = C.rows();
+	int j = C.cols();
 
 	std::cout << "test\n";
 	std::cout << "rows: "<< C.rows() << std::endl;
 	std::cout << "cols: "<< C.cols() << std::endl;
-	//std::cout << "rows: "<< *A.rows() << std::endl;
+	std::cout << "const A: "<< A.value() << std::endl;
 	
 	
 	//std::cout << "columms: "<< A.cols() << std::endl;
 	
 
-	/*for(size_t i=0; i < filas(a); i++){
+	for(size_t i=0; i < filas(a); i++){
 		for(size_t j=0; j < columnas(b); j++){
 			for(size_t k=0; k < columnas(a); k++){
 			c[i][j] += a[i][k] * b[k][j];
 			}			
 		}
-		}*/
-}
+		}
+}*/
 
 
 void MultMatrix::DOkij(const Matrix<float>& A,const Matrix<float>& B, Matrix<float>& C)
@@ -43,3 +45,21 @@ void MultMatrix::DOkij(const Matrix<float>& A,const Matrix<float>& B, Matrix<flo
 	//Completar
 }
 
+//Sin const
+void MultMatrix::DOijk(Matrix<float>& A, Matrix<float>& B, Matrix<float>& C)
+{
+	//Completar
+	//DO ijk
+	//int i = A.rows();
+	//int k = A.cols(); //Tambien B.rows()
+	//int j = B.cols();	
+
+	for(size_t i=0; i < A.rows(); i++){
+		for(size_t j=0; j < B.cols(); j++){
+			for(size_t k=0; k < A.cols(); k++){
+				//c[i][j] += a[i][k] * b[k][j];
+				C.value(i,j) += A.value(i,k)*B.value(k,j)
+			}			
+		}
+	}
+}
--- MultMatrix.hh
+++ MultMatrix.hh
@@ -22,7 +22,9 @@ public:			//Metodos
 	}
 	~MultMatrix(){}
 	
-	void DOijk(const Matrix<float>& A,const Matrix<float>& B, Matrix<float>& C);
+	//void DOijk(const Matrix<float>& A,const Matrix<float>& B, Matrix<float>& C);
+	//Sin const
+	void DOijk(Matrix<float>& A, Matrix<float>& B, Matrix<float>& C);
 	void DOkij(const Matrix<float>& A,const Matrix<float>& B, Matrix<float>& C);
 
 };
