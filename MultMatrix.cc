15901e89c697b713a749bdd6821396a81783cb3c
TheIszer <theiszer@gmail.com>
2022-05-13 12:36:35 -0400

    update const

--- MultMatrix.cc
+++ MultMatrix.cc
@@ -28,6 +28,9 @@ void MultMatrix::DOijk(const Matrix<float>& A,const Matrix<float>& B, Matrix<flo
 	std::cout << "rows: "<< C.rows() << std::endl;
 	std::cout << "cols: "<< C.cols() << std::endl;
 	//std::cout << "const A: "<< A.value() << std::endl;
+
+	//Matrix<float> Atemp(C.rows(), C.cols());
+	//Atemp << A;
 	
 	
 
