c3aeffdff9ffd4c915cf4ba38245689c104f0de6
TheIszer <theiszer@gmail.com>
2022-05-14 00:46:13 -0400

    update Matrix.hpp MultMatrix.cc

--- MultMatrix.cc
+++ MultMatrix.cc
@@ -24,32 +24,31 @@ void MultMatrix::DOijk(const Matrix<float>& A,const Matrix<float>& B, Matrix<flo
 	//int k = A.cols(); //Tambien B.rows()
 	//int j = B.cols();	
 
-	std::cout << "test\n";
-	std::cout << "rows: "<< C.rows() << std::endl;
-	std::cout << "cols: "<< C.cols() << std::endl;
-	std::cout << "const A value(0,0): "<< A.value(0,0) << std::endl;
-
-	//Matrix<float> Atemp(C.rows(), C.cols());
-	//Atemp << A;
-	
-	
-
-	//std::cout << "columms: "<< A.cols(0,0) << std::endl;
-	
-
-	/* for(size_t i=0; i < filas(a); i++){
-		for(size_t j=0; j < columnas(b); j++){
-			for(size_t k=0; k < columnas(a); k++){
-			c[i][j] += a[i][k] * b[k][j];
+	for(size_t i=0; i < A.rows(); i++){
+		for(size_t j=0; j < B.cols(); j++){
+			for(size_t k=0; k < A.cols(); k++){
+				//c[i][j] += a[i][k] * b[k][j];
+				C.value(i, j,  C.value(i, j)+A.value(i,k)*B.value(k,j)  );
 			}			
 		}
-	} */
+	}
+
 }
 
 
 void MultMatrix::DOkij(const Matrix<float>& A,const Matrix<float>& B, Matrix<float>& C)
 {
 	//Completar
+	for(size_t k=0; k < A.cols(); k++){
+		for(size_t i=0; i < A.rows(); i++){
+			//float r = a[i][k]
+			float r = A.value(i,k);
+				for(size_t j=0; j < B.cols(); j++){
+					//c[i][j] += r * b[k][j];
+					C.value(i,j, C.value(i, j)+r*B.value(k,j)  );
+				}			
+		}
+	}
 }
 
 //Sin const
